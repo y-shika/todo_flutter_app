c755f550ed4a603784a8a45d96e72c6ffc1015c2
y-shika <shikanai327327@gmail.com>
2020-01-14 02:13:54 +0900

    add: ２要素をリスト表示

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,18 @@ class TodoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello World"),
+      appBar: AppBar(
+        title: Text("TODO App"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("Tile_1"),
+          ),
+          ListTile(
+            title: Text("Tile_2"),
+          ),
+        ],
       ),
     );
   }
