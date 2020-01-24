17eecc11477882d37c7918cd377b17550303f807
y-shika <shikanai327327@gmail.com>
2020-01-24 23:56:52 +0900

    clean: main.dartからページ部を別ファイルに切り出した

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'todo.dart';
+import 'todo_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,29 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TODO App',
       home: TodoPage(),
-    );
-  }
-}
-
-/// TODOアプリのベースとなるページクラス
-class TodoPage extends StatelessWidget {
-  final todoListKey = new GlobalKey<TodoListState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("TODO App"),
-      ),
-      body: TodoList(key: todoListKey),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.add_circle),
-        label: Text("Add TODO"),
-        onPressed: () {
-          print("Press FloatingButton");
-          todoListKey.currentState.addTodoElement();
-        },
-      ),
     );
   }
 }
---
+++ lib/main/todo_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'todo.dart';
+
+/// TODOアプリのベースとなるページクラス
+class TodoPage extends StatelessWidget {
+  final todoListKey = new GlobalKey<TodoListState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TODO App"),
+      ),
+      body: TodoList(key: todoListKey),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.add_circle),
+        label: Text("Add TODO"),
+        onPressed: () {
+          print("Press FloatingButton");
+          todoListKey.currentState.addTodoElement();
+        },
+      ),
+    );
+  }
+}
