5541dc96d405ade440dfc26f060f7011cc3b852a
y-shika <shikanai327327@gmail.com>
2020-03-30 01:08:19 +0900

    add: Reduxではリビルドをある程度避けられないかもしれない旨をコメントとして残した

--- lib/redux/reducer/todo_list_reducer.dart
+++ lib/redux/reducer/todo_list_reducer.dart
@@ -12,6 +12,10 @@ List<TodoEntity> _addTodo(List<TodoEntity> todoList, AddTodoAction action) {
   return List.from(todoList)..add(action.todo);
 }
 
+// これだと1要素を反転させるだけなのに全要素に対してリビルドがかかる
+// しかしこれはReduxで構築する以上解決は難しいかもしれない
+// (参考にしていたリポジトリでも同様の書き方をしているため)
+// https://github.com/brianegan/flutter_architecture_samples/blob/master/redux/lib/reducers/todos_reducer.dart
 List<TodoEntity> _invertTodo(
     List<TodoEntity> todoList, InvertTodoAction action) {
   return todoList
