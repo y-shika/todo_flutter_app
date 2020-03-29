6ab67515738f175922eb33f04df08f268602dc18
y-shika <shikanai327327@gmail.com>
2020-03-29 22:39:33 +0900

    update: ReduxでTODOを追加

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:todo_flutter_app/models/todo_entity.dart';
 import 'package:todo_flutter_app/redux/action/actions.dart';
 import 'package:todo_flutter_app/redux/reducer/app_state_reducer.dart';
 import 'package:todo_flutter_app/redux/state/app_state.dart';
+import 'package:todo_flutter_app/ui/widgets/todo_element.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,27 +25,22 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('TODO App'),
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text('You have pushed the button this many times'),
-                StoreConnector<AppState, List<TodoEntity>>(
-                  converter: (store) => store.state.todoList,
-                  // TODO: ここではひとまずListの要素数を表示する 一区切りついたらList表示する
-                  builder: (context, todoList) => Text(
-                    'todoList length : ${todoList.length}',
-                    style: Theme.of(context).textTheme.display1,
-                  ),
-                ),
-              ],
+          body: StoreConnector<AppState, List<TodoEntity>>(
+            converter: (store) => store.state.todoList,
+            builder: (context, todoList) => ListView.builder(
+              itemCount: todoList.length,
+              itemBuilder: (context, index) {
+                return TodoElement(
+                  todo: todoList[index],
+                );
+              },
             ),
           ),
           floatingActionButton: StoreConnector<AppState, Function()>(
             converter: (store) {
-              return () => store.dispatch(AddTodoAction(const TodoEntity(
-                    'Title',
-                    'Detail',
+              return () => store.dispatch(AddTodoAction(TodoEntity(
+                    'Title${store.state.todoList.length}',
+                    'Detail${store.state.todoList.length}',
                   )));
             },
             builder: (context, callback) => FloatingActionButton(
---
+++ lib/ui/widgets/todo_element.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:todo_flutter_app/models/todo_entity.dart';
+
+/// TODO要素を表示するクラス
+class TodoElement extends StatelessWidget {
+  const TodoElement({this.todo});
+
+  final TodoEntity todo;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print('Tapped!');
+      },
+      onLongPress: () {
+        print('Long Pressed!');
+      },
+      child: Card(
+        child: Padding(
+          child: Text(
+            todo.title,
+            style: TextStyle(
+              color: Colors.white,
+              decoration: todo.active
+                  ? TextDecoration.none
+                  : TextDecoration.lineThrough,
+            ),
+          ),
+          padding: const EdgeInsets.all(20),
+        ),
+        color: todo.active ? Colors.blue : Colors.black12,
+      ),
+    );
+  }
+}
