20f3f3492d1bf2150a6f609f5cbfc2019dbeb601
y-shika <shikanai327327@gmail.com>
2020-03-31 17:39:08 +0900

    clean: 不要なクラスを削除して実装を綺麗にした

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:key_value_store_flutter/key_value_store_flutter.dart';
 import 'package:redux/redux.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_flutter_app/app.dart';
 import 'package:todo_flutter_app/redux/middleware/store_todo_middleware.dart';
 import 'package:todo_flutter_app/redux/reducer/app_state_reducer.dart';
 import 'package:todo_flutter_app/redux/state/app_state.dart';
-import 'package:todo_flutter_app/repositories/key_value_storage.dart';
 import 'package:todo_flutter_app/repositories/repository.dart';
 
 Future<void> main() async {
@@ -20,12 +17,7 @@ Future<void> main() async {
       // TODO: ここのinitialStateに`AppState.loading()`を入れたら良さそう
       //       最低限の機能を作り終えたらやる
       initialState: const AppState(todoList: []),
-      middleware: createStoreTodoMiddleware(LocalStorageRepository(
-        localStorage: KeyValueStorage(
-          'TodoApp',
-          FlutterKeyValueStore(await SharedPreferences.getInstance()),
-        ),
-      )),
+      middleware: createStoreTodoMiddleware(GithubRepositoryImpl()),
     ),
   ));
 }
--- lib/repositories/repository.dart
+++ lib/repositories/repository.dart
@@ -1,40 +1,28 @@
-import 'package:flutter/foundation.dart';
 import 'package:todo_flutter_app/models/github_result_dto.dart';
+import 'package:todo_flutter_app/repositories/http/github_http_client.dart';
 import 'package:todo_flutter_app/repositories/todo_entity.dart';
 import 'package:todo_flutter_app/repositories/todo_list_repository.dart';
 
-class LocalStorageRepository implements TodoListRepository {
-  const LocalStorageRepository({
-    @required this.localStorage,
-  });
-
-  final TodoListRepository localStorage;
-
-  // TODO: ひとまずasync, awaitは付けないでおく 問題が発生したら見直す
-  @override
-  Future<List<TodoEntity>> loadTodoList() {
-    // TODO: Debugメッセージ 後に消す
-    print('Debug: repository loadTodoList is called!');
-    return localStorage.loadTodoList();
-  }
-
-  // TODO: ひとまずasync, awaitは付けないでおく 問題が発生したら見直す
-  @override
-  Future saveTodoList(List<TodoEntity> todoList) {
-    // TODO: Debugメッセージ 後に消す
-    print('Debug: repository saveTodoList is called!');
-
-    return Future.wait<dynamic>([
-      localStorage.saveTodoList(todoList),
-    ]);
-  }
+// 試しのGithub apiを叩くための実装
+class GithubRepositoryImpl implements TodoListRepository {
+  final GithubHttpClient client = GithubHttpClient();
 
   @override
   Future<GithubResultDto> fetch() {
-    // TODO: Debugメッセージ 後に消す
-    print('Debug: repository fetch is called!');
+    return client.fetch();
+  }
 
-    // TODO: 非同期にしなくて良い？動かなかったりしたら調べてみる
-    return localStorage.fetch();
+  @override
+  Future<List<TodoEntity>> loadTodoList() {
+    // TODO: implement loadTodoList
+    print('Debug: GithubRepositoryImpl loadTodoList is called!');
+    return null;
+  }
+
+  @override
+  Future saveTodoList(List<TodoEntity> todoList) {
+    // TODO: implement saveTodoList
+    print('Debug: GithubRepositoryImpl saveTodoList is called!');
+    return null;
   }
 }
