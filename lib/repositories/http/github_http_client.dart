03c317f8a1a10f524e8613384997a418400fccee
y-shika <shikanai327327@gmail.com>
2020-04-24 18:31:11 +0900

    WIP: 自分で立てたAPIサーバのエンドポイントに接続するように変更

--- lib/repositories/http/github_http_client.dart
+++ lib/repositories/http/github_http_client.dart
@@ -7,7 +7,10 @@ class GithubHttpClient {
   static const _BASE_URL = 'https://api.github.com';
 
   Future<GithubResultDto> fetch() async {
-    final response = await http.get('$_BASE_URL/users/octocat');
+    // TODO: 自分で作成したapi (todo-golang-api) のクライアントクラスを作る
+    //       そのため下のコメントアウトは後に戻す
+    //final response = await http.get('$_BASE_URL/users/octocat');
+    final response = await http.get('https://todo-golang-api.herokuapp.com/api/github');
     final dynamic responseJson = json.decode(response.body);
     return GithubResultDto.fromJson(responseJson as Map<String, dynamic>);
   }
